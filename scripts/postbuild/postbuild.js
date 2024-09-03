bcc589e03f387b300c8a3ed7960db7a906390ceb
jamclean23 <jamclean23@gmail.com>
2024-09-03 10:59:05 -0400

    Add Google api key, prebuild will inject app.json from .env, post build scrubs it

---
+++ scripts/postbuild/postbuild.js
@@ -0,0 +1,36 @@
+// Runs after EAS build
+
+// ====== IMPORTS ======
+
+const fs = require('fs');
+const path = require('path');
+const logHeader = require('../shared/logHeader');
+
+const appJsonFileLoc = path.resolve(__dirname, '../../app.json');
+const appJson = require(appJsonFileLoc); 
+
+
+// ====== FUNCTIONS ======
+
+function postbuild () {
+    logHeader('\n====\nSTARTING POST BUILD SCRIPT');
+    scrubApiKeys();
+    writeAppJsonChanges();
+    logHeader('FINISHED POST BUILD\n====\n');
+}
+
+function scrubApiKeys () {
+    logHeader('REMOVING API KEYS FROM app.json...');
+    // Google Maps API key
+    appJson.expo.android.config.googleMaps.apiKey = "";
+    logHeader('DONE');
+}
+
+function writeAppJsonChanges () {
+    fs.writeFileSync(appJsonFileLoc, JSON.stringify(appJson, null, 4), 'utf8');
+}
+
+
+// ====== RUN ======
+
+postbuild();
---
+++ scripts/shared/logHeader.js
@@ -0,0 +1,3 @@
+module.exports = function logHeader (text) {
+    console.log(`\n\x1b[36m${text}\x1b[0m`);
+};
